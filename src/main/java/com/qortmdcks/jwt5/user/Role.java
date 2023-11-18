81e344dadff0887e1d55d8faacbff24d2c6bb964
baekseungchan1117 <bsc7386@gmail.com>
2023-11-18 18:05:08 +0900

    first commit

---
+++ src/main/java/com/qortmdcks/jwt5/user/Role.java
@@ -0,0 +1,7 @@
+package com.qortmdcks.jwt5.user;
+
+public enum Role {
+    USER,
+    MANAGER,
+    ADMIN
+}
---
+++ src/test/java/com/qortmdcks/jwt5/Jwt5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qortmdcks.jwt5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
